ec7ae546c1984c86a5f4c03dc49d9bde4c701fa1
masecla22 <supermasecla22@gmail.com>
2021-08-18 22:52:36 +0300

    Create RedditUser.java

---
+++ src/main/java/masecla/reddit4j/objects/RedditUser.java
@@ -0,0 +1,14 @@
+package masecla.reddit4j.objects;
+
+public class RedditUser extends RedditThing {
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
